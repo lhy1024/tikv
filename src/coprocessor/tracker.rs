c6a4ae3b1539dd8efe8e0c83422c2daea6b90a80
lhy1024 <admin@liudos.us>
2020-04-29 16:58:14 +0800

    update

--- src/coprocessor/tracker.rs
+++ src/coprocessor/tracker.rs
@@ -279,8 +279,8 @@ impl Tracker {
             .inc_by(self.total_perf_stats.0.decrypt_data_nanos as i64);
 
         tls_collect_scan_details(self.req_ctx.tag, &total_storage_stats);
-        tls_collect_read_flow(self.req_ctx.context.get_region_id(), &total_storage_stats);
-
+        let bytes=tls_collect_read_flow(self.req_ctx.context.get_region_id(), &total_storage_stats);
+        info!("cop";"elapsed"=>self.total_process_time,"bytes"=>bytes);
         let peer = self.req_ctx.context.get_peer();
         let region_id = self.req_ctx.context.get_region_id();
         let start_key = &self.req_ctx.lower_bound;
